82b03dffe1d6797a10fe5f058e5b0d23388292e2
Digi <digiworm0@gmail.com>
2025-11-24 11:57:43 -0600

    Fix Build Error

--- src/components/inventory/InventoryNavBar.tsx
+++ src/components/inventory/InventoryNavBar.tsx
@@ -2,7 +2,7 @@ export interface InventoryNavBarProps {
     appID: number;
 }
 
-export default function InventoryNavBar(props: InventoryNavBarProps) {
+export default function InventoryNavBar(_props: InventoryNavBarProps) {
     return (
         <div className={"wizard_inventory"}>
             {/*<BulkInventoryButton appID={props.appID}/>*/}
